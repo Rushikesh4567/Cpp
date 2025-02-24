4cecc7aeab0d742f5ed1952d62aac5d283e69bcd
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-02-24 20:02:17 +0530

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Book{
+    private:
+    string title;
+    string author;
+    int page_count[200];
+
+    public:
+        Book()
+        {
+            title = "un"
+        }
+
+}
